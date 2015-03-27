7eba755d23285ae788a94e1d0fb279d442b03854
Daniel Luker <daniel.luker@duke.edu>
2015-03-27 02:42:41 -0400

    Added daniel interface example

---
+++ src/XMLBuilder.java
@@ -0,0 +1,53 @@
+import java.io.File;
+
+import javafx.scene.Node;
+
+/***
+ * This is an interface. It represents a class which will be used to write the
+ * elements from the class into an xml file, to be used by the player.
+ * 
+ * @author Daniel Luker
+ *
+ */
+interface XMLBuilder {
+
+	/***
+	 * Returns all of the elements (children) of a given key.
+	 * 
+	 * @param name
+	 * @param value
+	 * @return
+	 */
+	abstract Node getElements(String name, String value);
+
+	/***
+	 * Puts a value into the file
+	 * 
+	 * @param name
+	 * @param value
+	 */
+	abstract void add(String name, String value);
+
+	/***
+	 * Updates a value
+	 * 
+	 * @param name
+	 * @param value
+	 */
+	abstract void update(String name, String value);
+
+	/***
+	 * Removes a file from the map
+	 * 
+	 * @param name
+	 * @param value
+	 */
+	abstract void remove(String name, String value);
+
+	/***
+	 * Writes out the result of the generated xml file
+	 * 
+	 * @return a handle to the file object generated.
+	 */
+	abstract File write();
+}
