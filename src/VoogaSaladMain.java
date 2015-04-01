bbc151264d590fcaf50b12f94535af1ff14eed80
Daniel Luker <daniel.luker@duke.edu>
2015-04-01 09:50:53 -0400

    Fixed window size to be dynamic on the size of the screen.

--- src/VoogaSaladMain.java
+++ src/VoogaSaladMain.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -7,34 +9,34 @@ import javafx.stage.Stage;
  * @author Brian Lavallee
  * @since 31 March 2015
  */
-public class VoogaSaladMain extends Application
-{
-    private static final int HEIGHT = 1000;
-    private static final int WIDTH = 1800;
-    
-    /**
-     * Initializes the stage, creates the first scene, and displays the application to the use.
-     */
-    public void start(Stage stage) throws Exception
-    {
-	stage.setTitle("VoogaSalad");
-	stage.setHeight(HEIGHT);
-	stage.setWidth(WIDTH);
-	stage.setResizable(false);
-	
-	MainMenu menu = new MainMenu(WIDTH, HEIGHT);
-	stage.setScene(menu.initialize());
-	
-	stage.show();
-    }
-    
-    /**
-     * Launches the application.
-     * 
-     * @param args	are generally empty.
-     */
-    public static void main(String[] args)
-    {
-	launch(args);
-    }
+public class VoogaSaladMain extends Application {
+
+	/**
+	 * Initializes the stage, creates the first scene, and displays the
+	 * application to the use.
+	 */
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("VoogaSalad");
+		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+		stage.setX(primaryScreenBounds.getMinX());
+		stage.setY(primaryScreenBounds.getMinY());
+		stage.setWidth(primaryScreenBounds.getWidth());
+		stage.setHeight(primaryScreenBounds.getHeight());
+		stage.setResizable(false);
+
+		MainMenu menu = new MainMenu((int)primaryScreenBounds.getWidth(), (int)primaryScreenBounds.getHeight());
+		stage.setScene(menu.initialize());
+
+		stage.show();
+	}
+
+	/**
+	 * Launches the application.
+	 * 
+	 * @param args
+	 *            are generally empty.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
