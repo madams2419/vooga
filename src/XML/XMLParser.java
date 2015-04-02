cbed6dc87f5d6bfd6ab241ef7e26f1f609dc52c5
Hojeannie Chung <hc114@duke.edu>
2015-04-02 15:49:52 -0400

    trying to get a buttomfactory going by pulling a file from the XML and enumerating the properties from an XML also

--- src/XML/XMLParser.java
+++ src/XML/XMLParser.java
@@ -1,57 +1,73 @@
 package XML;
 
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import javax.xml.parsers.DocumentBuilder;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class XMLParser {
-	private static String Label = null;
-
-	public Map<String,String> parse(File f, String s) throws Exception {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Map<String,String> parsedMap=new HashMap<String,String>();
-		Document document = builder.parse(f);
-		document.getDocumentElement().normalize();
-		NodeList nList = document.getElementsByTagName(String.format("resources/%s",s));
-
-		for (int temp = 0; temp < nList.getLength(); temp++) {
-			Node node = nList.item(temp);
-			
-			if (node.getNodeType() == Node.ELEMENT_NODE) {
-				Element eElement = (Element) node;
-				Label = eElement.getNodeName().trim();
-			}
-			
-			parsedMap.put(Label, "Button");
-		}
-
-		// write it back onto the XML File
-		TransformerFactory transformerFactory = TransformerFactory
-				.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(document);
-		StreamResult result = new StreamResult(f);
-		transformer.transform(source, result);
-		System.out.println(parsedMap);
-
-		return parsedMap;
-	}
+	public static Map<String,String> mAttributeMap;
+	public static File mFile;
 	
+	public static File parse(String f, String s) {
+
+		try {
+			File fXmlFile = new File(f);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			//optional, but recommended
+			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			doc.getDocumentElement().normalize();
+
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+			NodeList nList = doc.getElementsByTagName("Button");
+
+			System.out.println("----------------------------");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+
+				Node nNode = nList.item(temp);
+				
+				System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+
+
+					/*This is the element we are looking for (i.e.)"button, box, etc."*/					
+					if(eElement.getNodeName().equals(s)){
+						mAttributeMap=new HashMap<String,String>();
+						if(eElement.hasAttribute("file")){
+							mFile=new File(eElement.getAttribute("file"));
+						}
+						
+						
+						for(int i=0; i<eElement.getAttributes().getLength(); i++){
+							Node attr=eElement.getAttributes().item(i);
+							String attrName=attr.getNodeName();
+							String attrValue=attr.getNodeValue();	
+							mAttributeMap.put(attr.getNodeName(), attr.getNodeValue());
+						}
+					}
+					System.out.println(mAttributeMap);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mFile;
+	}
+
 
 }
--- src/authoring_UserInterface/ButtonFactory.java
+++ src/authoring_UserInterface/ButtonFactory.java
@@ -3,16 +3,29 @@
  */
 package authoring_UserInterface;
 
-import java.util.HashMap;
+import java.io.File;
 import java.util.Map;
+
+
+
+
+
+import org.w3c.dom.Node;
+
 import XML.XMLParser;
 /**
  * @author hojeanniechung
  *
  */
 public class ButtonFactory {
-	XMLParser xmlParse=new XMLParser();
-	File f=new File();
-	Map<String,String> LabeltoTypeMap=xmlParse();
-
+	static String[] a;
+	
+	public static void main(String[] args){
+		Map<String, String> Labels;
+		String s="Button"; //test Case
+		String f="src/Resources/ButtonTest.xml";
+		File buttonFile=XMLParser.parse(f, s);
+		Labels=XMLParser.mAttributeMap;
+	}
+	
 }
