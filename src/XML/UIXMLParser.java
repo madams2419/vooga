1a4b0684d98a22cd620e10837e87e8d630969fd2
mf108 <mf108@duke.edu>
2015-04-03 17:01:35 -0400

    LayoutXMLParser

--- src/XML/UIXMLParser.java
+++ src/XML/UIXMLParser.java
@@ -1,17 +1,17 @@
 package XML;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class UIXMLParser {
 	public static File mFile;
