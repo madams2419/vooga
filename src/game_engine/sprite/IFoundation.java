38fb82027a5df34be8f9da5c7ae1ca2f00ca04c8
Emre Sonmez <esonmez@me.com>
2015-04-04 15:00:56 -0400

    fixed merge conflicts

--- src/game_engine/sprite/IFoundation.java
+++
@@ -1,8 +0,0 @@
-package game_engine.sprite;
-
-public interface IFoundation {
-	double alignUpperEdge(double y);
-	double alignLowerEdge(double y);
-	double alignLeftEdge(double x);
-	double alignRightEdge(double x);
-}
