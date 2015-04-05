a591fe93c108bbbebda67e257e789d287ac7aef0
Daniel Luker <daniel.luker@duke.edu>
2015-04-05 16:56:58 -0400

    Fixed merge conflicts

--- src/game_engine/Objective.java
+++ src/game_engine/Objective.java
@@ -5,28 +5,31 @@ import java.util.List;
 import java.util.Observable;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
-import java.util.function.Consumer;
 
 import javafx.beans.property.BooleanProperty;
 
-/**@author Tony
- * Goal that leads to a specific action when completed
+/**
+ * @author Tony Goal that leads to a specific action when completed
  */
 public class Objective {
 
 	List<Objective> subObjectives;
 	boolean condition;
-	
-        /**
-=======
-		/**
->>>>>>> 25435599e392a0b513a380c08dd91c54a2744e12
-         * Constructor
-         * @param condition BooleanProperty Represents a condition for the objective completion
-         * @param subObjectives List<Objective> List of objectives that must be completed first
-         * @param action Consumer<?> Action that occurs when the objective is complete
-         */
-	public Objective(BooleanProperty condition, List<Objective> subObjectives, Consumer<?> action) {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param condition
+	 *            BooleanProperty Represents a condition for the objective
+	 *            completion
+	 * @param subObjectives
+	 *            List<Objective> List of objectives that must be completed
+	 *            first
+	 * @param action
+	 *            Consumer<?> Action that occurs when the objective is complete
+	 */
+	public Objective(BooleanProperty condition, List<Objective> subObjectives,
+			Consumer<?> action) {
 		// TODO
 	}
 
@@ -34,79 +37,84 @@ public class Objective {
 	 * Empty constructor for mockObjective in usecases
 	 */
 	public Objective() {
-			
+
 	}
 
 	/**
 	 * Method setCondition.
-	 * @param condition BooleanProperty
+	 * 
+	 * @param condition
+	 *            BooleanProperty
 	 */
 	public void setCondition(BooleanProperty condition) {
 		// TODO
 	}
-	
+
 	/**
 	 * Method setSubObjectivies.
-	 * @param subObjectives List<Objective>
+	 * 
+	 * @param subObjectives
+	 *            List<Objective>
 	 */
 	public void setSubObjectivies(List<Objective> subObjectives) {
 		// TODO
 	}
-	
+
 	/**
 	 * Method addSubOjective
-	 * @param objective Objective
+	 * 
+	 * @param objective
+	 *            Objective
 	 */
-	public void addSubObjective (Objective objective){
-	    
+	public void addSubObjective(Objective objective) {
+
 	}
-	
+
 	/**
-	 * Method removeSubObjective
-	 * Removes a subobjective if present else do nothing
-	 * @param objective Objective
+	 * Method removeSubObjective Removes a subobjective if present else do
+	 * nothing
+	 * 
+	 * @param objective
+	 *            Objective
 	 */
-	public void removeSubObjective (Objective objective){
-	    
+	public void removeSubObjective(Objective objective) {
+
 	}
-	
+
 	/**
-	 * Method addParent
-	 * Adds a parent objective. The parent objective has this objective object as a subobjective.
+	 * Method addParent Adds a parent objective. The parent objective has this
+	 * objective object as a subobjective.
 	 */
-	public void addParent(Objective parent){
-	    
+	public void addParent(Objective parent) {
+
 	}
-	
+
 	/**
-	 * Method start
-	 * Start working towards completion of the objective.
+	 * Method start Start working towards completion of the objective.
 	 */
 	public void start() {
 		// TODO
 	}
-	
+
 	/**
-	 * method onComplete
-	 * On completion of an objective, perform action
+	 * method onComplete On completion of an objective, perform action
 	 */
 	public void onComplete() {
 		// TODO
 	}
-	
+
 	/***
 	 * To check whether this objective has been completed
+	 * 
 	 * @return True iff the objective has been completed
 	 */
 	public boolean isComplete() {
 		// TODO
 		return false;
 	}
-	
 
 	public <T extends Observable> void setPredicate(Predicate<T> condition) {
 		// TODO
 	}
-	
-}
 
+}
