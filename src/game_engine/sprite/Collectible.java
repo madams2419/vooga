1ee0d764840c01ab78af5dd8e964ce14db398bf7
Kevinchang1214 <kgc12@duke.edu>
2015-04-05 14:25:37 -0400

    added constructors to sprite subclasses

--- src/game_engine/sprite/Collectible.java
+++ src/game_engine/sprite/Collectible.java
@@ -2,6 +2,18 @@ package game_engine.sprite;
 
 public class Collectible extends Sprite{
 
+    public Collectible() {
+        super();
+      }
+
+   
+      public Collectible(String name){
+          super(name);
+      }
+    
+      public Collectible(String name, double id){
+          super(name,id);
+      }
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
