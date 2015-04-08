99da30f458dc3e90415499a1f3b5d627776d9f28
Hojeannie Chung <hc114@duke.edu>
2015-04-08 18:23:48 -0400

    implemented buttonGenerator, (use patterns?)

--- src/authoring/userInterface/ButtonFactory.java
+++ src/authoring/userInterface/ButtonFactory.java
@@ -6,16 +6,19 @@ package authoring.userInterface;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
 
 import javafx.scene.control.Button;
 
 import org.w3c.dom.Node;
+
 import XML.LayoutXMLParser;
 /**
  * @author hojeanniechung
  *
  */
 public class ButtonFactory {
+	private static final boolean String = false;
 	private static File mFile;
 	private static String mType;
 	private static ButtonFactory mInstance;
@@ -25,49 +28,27 @@ public class ButtonFactory {
 
 	/*==============================Constructors=================================================*/
 
-	public static ButtonFactory getSharedInstace(String f) {
+	public static ButtonFactory getSharedInstace() {
 		if(mInstance==null)
-			mInstance=new ButtonFactory(f);
+			mInstance=new ButtonFactory();
 		return mInstance;
 		// TODO Auto-generated constructor stub
 	}
-
-	public static ButtonFactory getSharedInstance() {
-		return mInstance;
-	}
-
-	//	public static void main(String[] args){	
-	//		s="Button"; //test Case
-	//		String f="src/Resources/FilestoParse.xml";
-	//		LayoutXMLParser.parse(f, s);
-	//		mFile=LayoutXMLParser.getFile(); 
-	//	}
-	//	
 	
 	public ButtonFactory(){
 		System.out.println("Reached public constructor for ButtonFactory");
 	}
-	private ButtonFactory(String f){
-		//s="Button"; //test Case
-		f="src/settings/layout.xml";
-		LayoutXMLParser.parse(f);
-		mFile=LayoutXMLParser.getFile();
-		GetAttributes();
 
-	}
-
-	public static ArrayList<Map> GetAttributes(){	
-		//clear AttributeList from before
-		for(int i=0; i<LayoutXMLParser.mAttributesList.size(); i++){
-			LayoutXMLParser.mAttributesList.remove(i);
-		}
-		LayoutXMLParser.parse(mFile.toString(), mType);
-		mAttributesList= LayoutXMLParser.mAttributesList;
-		//System.out.println("mAttributesList is "+mAttributesList);
-		return mAttributesList;
-	}
-
-	public static void generateButton(Map m){
+	public static void generateButton(Map<String,String> m){
 		System.out.println("button" + m);
+//		ArrayList<String> Keys=new ArrayList<String>();
+//		for(String mapKeys :m.keySet()){
+//			if(!Keys.contains(mapKeys)){
+//				Keys.add(mapKeys);
+//			}
+//		}
+		Button b=new Button(m.get("label"));
+		b.setOnAction(e->m.get("listener"));
+		mButtonList.add(b);
 	}
 }
