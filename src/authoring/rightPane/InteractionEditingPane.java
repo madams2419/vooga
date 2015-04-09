d931da268acac67c68d9506a34eaf8df3e614205
BrianLavallee <bpl6@duke.edu>
2015-04-09 18:22:13 -0400

    getting patches

--- src/authoring/rightPane/InteractionEditingPane.java
+++ src/authoring/rightPane/InteractionEditingPane.java
@@ -35,7 +35,7 @@ public class InteractionEditingPane extends EditingPane {
                                        .format("This will contain the interactions")));
 
         // temporary
-        sprite1 = new Sprite(1, "/images/turtle.png");
+        sprite1 = new Sprite(1, "/images/smallLuigi.png");
         sprite2 = new Sprite(1, "/images/luigi.png");
 
         addSpriteAndComboBoxToPane(sprite1, actionPossibilities);
