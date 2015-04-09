3b91d6851a2b20083806d60864bea97eff0e7609
Natalie Chanfreau <nataliechanfreau@gmail.com>
2015-04-09 18:53:50 -0400

    trying to unrevert game_engine things

--- src/game_engine/control/AddBehavior.java
+++ src/game_engine/control/AddBehavior.java
@@ -2,10 +2,10 @@ package game_engine.control;
 
 import game_engine.*;
 
-public class AddBehavior implements Behavior{
+public class AddBehavior implements IBehavior{
 	
 	@Override
-	public void execute() {
+	public void execute(double[] params) {
 		ControlTester.addTrack();
 		ControlTester.updateText();
 	}
