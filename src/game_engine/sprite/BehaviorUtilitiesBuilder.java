b6990f8b5dfd1037d0ff75f404a6cee567d4fe99
Brian Lavallee <bpl6@duke.edu>
2015-04-10 01:00:41 -0400

    Update BehaviorUtilitiesBuilder.java

--- src/game_engine/sprite/BehaviorUtilitiesBuilder.java
+++ src/game_engine/sprite/BehaviorUtilitiesBuilder.java
@@ -3,13 +3,13 @@ import java.lang.reflect.Method;
 
 public class BehaviorUtilitiesBuilder {
 	public void tester(){
-		Method[] myMethods = Character.class.
-		for(Method m:myMethods){
-			String className = m.toString();
+//		Method[] myMethods = Character.class.
+//		for(Method m:myMethods){
+//			String className = m.toString();
 //			if(className.contains("IBehavior")){
-				System.out.println(m.toString());
+//				System.out.println(m.toString());
 //			}
-		}
+//		}
 	}
 	
 	public static void main (String[] args){
