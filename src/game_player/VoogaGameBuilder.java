ff542afd0faf04fcef114af8c8a5924f9c8c9f0b
BrianLavallee <bpl6@duke.edu>
2015-04-12 20:36:12 -0400

    missed a merge conflict in simple.game

--- src/game_player/VoogaGameBuilder.java
+++ src/game_player/VoogaGameBuilder.java
@@ -49,7 +49,7 @@ public class VoogaGameBuilder {
             Level level = buildLevel(directory);
             game.addLevel(level);
         }
-        game.setActiveLevel(Integer.parseInt(parser.getValue("start")));
+       game.setActiveLevel(0);
         
         return game;
     }
