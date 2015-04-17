b84260d39dfdabb8cd703d593a41cffc303e53cd
Michael Lee <gl83@duke.edu>
2015-04-17 16:02:59 -0400

    collision composition

---
+++ src/game_engine/collision/ICollision.java
@@ -0,0 +1,9 @@
+package game_engine.collision;
+
+public interface ICollision {
+	
+	public void execute();
+	
+	public boolean isColliding();
+
+}
---
+++ src/game_engine/collision/PixelPerfectCollision.java
@@ -0,0 +1,22 @@
+package game_engine.collision;
+
+public class PixelPerfectCollision implements ICollision{
+
+	public PixelPerfectCollision() {
+	//Pixel Perfect Collisons
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isColliding() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
