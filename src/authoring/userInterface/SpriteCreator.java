b590b3de47bf031d693426b2f3b6d9c26defa72d
Hojeannie Chung <hc114@duke.edu>
2015-04-17 16:01:19 -0400

    fixing spritesheet classes

--- src/authoring/userInterface/SpriteCreator.java
+++ src/authoring/userInterface/SpriteCreator.java
@@ -1,32 +1,55 @@
 package authoring.userInterface;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import javax.swing.JFrame;
 import java.util.ArrayList;
 
-public class SpriteCreator {
+
+public class SpriteCreator extends JFrame {
+	BufferedImage sprite;
 	private static final int WIDTH =512;
-    private static final int HEIGHT=512;
+	private static final int HEIGHT=512;
 	private BufferedImage image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
 	private BufferedImage spriteSheet=null;
 
-    //Creating an ArrayList of Players based on spritesheets
-    private BufferedImage player;
-    private ArrayList<BufferedImage> Player=new ArrayList<>();
-    private void init(){
-    	BufferedImageLoader loader=new BufferedImageLoader();
-    	try{
-    		spriteSheet=loader.loadImage("/Users/hojeanniechung/Documents/workspace_308/voogasalad_HighScrollers/res/bouncy_ball.jpg");
-    	}catch(IOException e){
-    		e.printStackTrace();
-    	}
-    	SpriteSheet ss = new SpriteSheet(spriteSheet);
-    	for(int i=0; i<WIDTH/60; i++){
-    		for(int j=0; j<HEIGHT/60; j++){
-    			Player.add(ss.grabImage(i,j,60,60));
-    		}
-    	}    
-    	System.out.println(Player.toString());
-    }
-    
+	//Creating an ArrayList of Players based on spritesheets
+	private BufferedImage player;
+	private ArrayList<BufferedImage> Player=new ArrayList<>();
+
+	public SpriteCreator() { 
+		setSize(600, 500); 
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		init();
+	}
+
+
+	private void init() {  
+		BufferedImageLoader loader = new BufferedImageLoader();
+		BufferedImage spriteSheet = null;  
+		try {   
+			spriteSheet = loader.loadImage("/Users/hojeanniechung/Documents/workspace_308/voogasalad_HighScrollers/res/testSpritesheet.png"); 
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} 
+		SpriteSheet ss = new SpriteSheet(spriteSheet);  
+		sprite = ss.grabSprite(0, 0, 20, 20);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(sprite, 100, 100, 50, 50, null);
+		repaint();
+	}
+
+	public static void main(String[] args) {
+		SpriteCreator main = new SpriteCreator();
+	}
 }
+
+
+
--- src/authoring/userInterface/SpriteSheet.java
+++ src/authoring/userInterface/SpriteSheet.java
@@ -1,23 +1,16 @@
-/**
- * This will allow the user to select from a number of images (or insert his/her
- * own image) to create a character that can be placed on the canvas.
- * 
- * @author hojeannie Chung
- *
- */
-
 package authoring.userInterface;
 
 import java.awt.image.BufferedImage;
-
 public class SpriteSheet {
-	private BufferedImage image;
-	public SpriteSheet(BufferedImage image){
-		this.image=image;
+
+	public BufferedImage spriteSheet;
+
+	public SpriteSheet(BufferedImage ss) {
+		this.spriteSheet = ss;
 	}
-	
-	public BufferedImage grabImage(int col, int row, int width, int height){
-		BufferedImage img=image.getSubimage((col*60)-60,(row*60)-60,width,height);
-		return img;
+
+	public BufferedImage grabSprite(int x, int y, int width, int height) {
+		BufferedImage sprite = spriteSheet.getSubimage(x, y, width, height);
+		return sprite;
 	}
 }
