a335ba51ae6c84fd01f53eb415f4b6f133975ace
Michael Lee <gl83@duke.edu>
2015-04-18 15:40:57 -0400

    collision resolver/detector interfaces

--- src/game_engine/collision/PixelPerfectDetector.java
+++ src/game_engine/collision/PixelPerfectDetector.java
@@ -1,80 +1,22 @@
 package game_engine.collision;
 
-import game_engine.behaviors.IBehavior;
-import game_engine.physics.PhysicsCollision;
-import game_engine.physics.PhysicsCollisionFactory;
-import game_engine.sprite.Sprite;
 import javafx.scene.image.ImageView;
+import game_engine.sprite.Sprite;
 
-/**
- * 
- * 
- * @author Michael Lee
- *
- */
+public class PixelPerfectDetector implements ICollisionDetector{
 
-public class Collision {
-	private Sprite spriteA;
-	private Sprite spriteB;
-//	private Map<Sprite, Map<IBehavior, String[]>> behaviorList;
-	private IBehavior behavior;
-	private CollisionDirection direction;
-	private boolean isRealistic;
-	private PhysicsCollision pCollision;
 
-	/**
-	 * 
-	 * @param a
-	 * @param b
-	 * @param behave
-	 * @param d
-	 * @param real
-	 * @param p
-	 */
-	public Collision(Sprite a, Sprite b, IBehavior behave, CollisionDirection d, boolean real) {
-		spriteA = a;
-		spriteB = b;
-		behavior = behave;
-		direction = d;
-		isRealistic = real;
-		pCollision = null;
-	}
+	public boolean isColliding(Sprite a, Sprite b) {
+		boolean[][] bitMapA = a.getHitBox().getBitMap();
+		boolean[][] bitMapB = b.getHitBox().getBitMap();
 
-	public void getColliding() {
-//		spriteList.stream().filter(sprite -> collidingHitBox(sprite, spriteA)).filter(sprite->collide(sprite,spriteA))
-//				.forEach(this::execute);
-		//if(collidingHitBox() && collide()){ //TODO collide() throws null pointer exceptions
-		if(collidingHitBox()){
-			execute();
-			
-			if(isRealistic){
-				pCollision.resolve();
-			}
-		}
-		
-	}
-
-	private void execute() {
-		behavior.perform();
-	}
-	
-	private boolean collidingHitBox(){
-		pCollision = PhysicsCollisionFactory.getCollision(spriteA, spriteB);
-		return pCollision.collide();
-	}
-
-	private boolean collide() {
-
-		boolean[][] bitMapA = spriteA.getHitBox().getBitMap();
-		boolean[][] bitMapB = spriteB.getHitBox().getBitMap();
-
-		ImageView s1 = spriteA.getImageView();
+		ImageView s1 = a.getImageView();
 		double aLeft = s1.getX();
 		double aTop = s1.getY();
 		double aRight = s1.getX() + s1.getImage().getWidth();
 		double aBot = s1.getY() + s1.getImage().getHeight();
 
-		ImageView s2 = spriteB.getImageView();
+		ImageView s2 = b.getImageView();
 		double bLeft = s2.getX();
 		double bTop = s2.getY();
 		double bRight = s2.getX() + s2.getImage().getWidth();
@@ -102,16 +44,16 @@ public class Collision {
 
 		boolean[][] collisionMap = isColliding(mapA, mapB);
 
-		for(boolean[] bList: collisionMap){
-			for(boolean b: bList){
-				if(b){
+		for(boolean[] boolList: collisionMap){
+			for(boolean bool: boolList){
+				if(bool){
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-
+	
 	private boolean[][] spliceBitMap(boolean[][] bitMap, int startX, int endX,
 			int startY, int endY) {
 
@@ -124,7 +66,6 @@ public class Collision {
 		}
 		return bitSplice;
 	}
-
 	
 	private boolean[][] isColliding(boolean[][] mapA, boolean[][] mapB) {
 		boolean[][] bitMap = new boolean[mapA.length][mapA[0].length];
@@ -134,4 +75,7 @@ public class Collision {
 
 		return bitMap;
 	}
+
+
+
 }
