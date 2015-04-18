181d9771131704054b369feb86bc1877ae5f33ec
Emre Sonmez <esonmez@me.com>
2015-04-18 18:14:21 -0400

    cleaning physics refactor

--- src/game_engine/physics_engine/PhysicsCollisionFactory.java
+++ src/game_engine/physics_engine/PhysicsCollisionFactory.java
@@ -5,23 +5,5 @@ import game_engine.physics.RigidBody.RBodyType;
 import game_engine.physics_engine.physics_object.IPhysicsObject;
 
 public class PhysicsCollisionFactory {
-
-	public static PhysicsCollision getCollision(IPhysicsObject poA, IPhysicsObject poB) {
-		RigidBody rbA = poA.getRigidBody();
-		RigidBody rbB = poB.getRigidBody();
-
-		if(rbA.getType() == RBodyType.CIRCLE) {
-			if(rbB.getType() == RBodyType.CIRCLE) {
-				return new CircleCircleCollision(poA, poB);
-
-			} else {
-				return new CircleRectCollision(poA, poB);
-			}
-		} else if(rbA.getType() == RBodyType.RECTANGLE && rbB.getType() == RBodyType.RECTANGLE) {
-				return new RectRectCollision(poA, poB);
-		} else {
-			return getCollision(b, a);
-		}
-	}
-
+	// TODO fill this out
 }
--- src/game_engine/physics_engine/physics_object/IPhysicsObject.java
+++ src/game_engine/physics_engine/physics_object/IPhysicsObject.java
@@ -2,6 +2,8 @@ package game_engine.physics_engine.physics_object;
 
 /**
  * physics object interface
+ * note: we have included multiple implementations of IPhysicsObjects (simple and complex)
+ * to illustrate the flexibility of our game engine
  */
 public interface IPhysicsObject {
 	public abstract void update();
