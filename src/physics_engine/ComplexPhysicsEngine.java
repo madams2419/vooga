afe8006263e16afa43e773ba3d36fa471355bffa
Emre Sonmez <esonmez@me.com>
2015-04-18 17:16:26 -0400

    physics engine intial refactoring

---
+++ src/physics_engine/ComplexPhysicsEngine.java
@@ -0,0 +1,38 @@
+package physics_engine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import game_engine.sprite.Sprite;
+
+public class ComplexPhysicsEngine implements IPhysicsEngine{
+	private List<IPhysicsObject> physicsObject;
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPhysicsObject(IPhysicsObject physicsObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePhysicsObject(IPhysicsObject physicsObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resolveCollision(IPhysicsObject physicsObject1,
+			IPhysicsObject physicsObject2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/physics_engine/IPhysicsObject.java
@@ -0,0 +1,5 @@
+package physics_engine;
+
+public interface IPhysicsObject {
+
+}
