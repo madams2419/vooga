02b82cbf3f0f3a5b0539869b9c9425652a26e6be
Natalie Chanfreau <nataliechanfreau@gmail.com>
2015-04-22 01:00:33 -0400

    small change - changed textbox to label

--- src/authoring/panes/rightPane/CreationPane.java
+++ src/authoring/panes/rightPane/CreationPane.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.util.List;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -23,6 +23,7 @@ import authoring.util.ImageEditor;
  */
 
 public class CreationPane extends EditingPane {
+    private static final String HELP_LABEL_CONTENT = "Click on one of the images to select it.";
     private List<String> myAvailableTypeURIs;
     private VBox myVBox;
     private static final String buttonLabel = "+";
@@ -33,17 +34,23 @@ public class CreationPane extends EditingPane {
 
     CreationPane (Scene scene, RightPane parent, List<String> availableTypeURIs) {
         super(scene, parent);
-        this.getChildren().add(
-                               new TextArea(String
-                                       .format("Information%n"
-                                               + "when a drop down%n" + "item is selected, or%n"
-                                               + "a current element on%n" + "the scroll pane is%n"
-                                               + "selected (up to two%n"
-                                               + "selections), its (their)%n"
-                                               + "information will be%n" + "shown here.")));
+//        this.getChildren().add(
+//                               new TextArea(String
+//                                       .format("Information%n"
+//                                               + "when a drop down%n" + "item is selected, or%n"
+//                                               + "a current element on%n" + "the scroll pane is%n"
+//                                               + "selected (up to two%n"
+//                                               + "selections), its (their)%n"
+//                                               + "information will be%n" + "shown here.")));
+        addHelpLabel();
         myAvailableTypeURIs = availableTypeURIs;
         addSpritesToPane();
         addButtonToPane(buttonLabel);
+    }
+
+    private void addHelpLabel () {
+        Label label = new Label(HELP_LABEL_CONTENT);
+        getChildren().add(label);
     }
 
     private void addButtonToPane (String buttonLabel) {
