7d7ec0cee517f3ff5b84e0677a6268c5713b5fdd
Daniel Luker <daniel.luker@duke.edu>
2015-04-24 16:06:55 -0400

    Finished the Objective dialog, working on incorporating the changes into the xmlbuilder

--- src/authoring/panes/rightPane/LevelSettingPane.java
+++ src/authoring/panes/rightPane/LevelSettingPane.java
@@ -27,11 +27,11 @@ public class LevelSettingPane extends EditingPane {
     public void updateLevels(RightPane parent){
         this.getChildren().clear();
         /* Default Map */
-        Iterator<List<CenterCanvas>> it = myParent.getParent().getCenterPane().getMaps();
+        List<List<CenterCanvas>> it = myParent.getParent().getCenterPane().getMaps();
         int levelIndex = 0, mapIndex = 0;
         HBox h = new HBox(5);
-        while(it.hasNext()){
-            List<CenterCanvas> maps = it.next();
+        for(int i = 0; i < it.size(); i++){
+            List<CenterCanvas> maps = it.get(i);
             Label levelLabel = new Label("Level "+ levelIndex++);
             ComboBox<String> mapBox = new ComboBox<String>();
             mapBox.setPromptText("Select Map");
