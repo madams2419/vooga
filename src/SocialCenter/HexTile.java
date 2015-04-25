c7779a1d75c3ae9f12a3ef0a8d17ba5d81528240
Michael Lee <gl83@duke.edu>
2015-04-25 00:38:11 -0400

    hexPage

--- src/SocialCenter/HexTile.java
+++ src/SocialCenter/HexTile.java
@@ -2,6 +2,8 @@ package SocialCenter;
 
 import java.util.Arrays;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
@@ -15,37 +17,43 @@ public class HexTile {
 	public HexTile(double x, double y, double side) {
 		double[] points = new double[12];
 		connectionPoints = new double[12];
-		
-		for (int i = 0; i < POINTS; i +=2) {
+
+		for (int i = 0; i < POINTS; i += 2) {
 			double newX = x + side * Math.cos(Math.toRadians(startAngle));
 			double newY = y - side * Math.sin(Math.toRadians(startAngle));
 			points[i] = newX;
 			points[i + 1] = newY;
+			connectionPoints[i] = newX + side * Math.cos(Math.toRadians(startAngle + HEX));
+			connectionPoints[i + 1] = newY- side * Math.sin(Math.toRadians(startAngle + HEX));;
 			startAngle += HEX;
 		}
+
+//		for (int i = 0; i < POINTS; i += 2) {
+//			double newX = points[i] + side
+//					* Math.cos(Math.toRadians(startAngle + HEX));
+//			double newY = points[i + 1] - side
+//					* Math.sin(Math.toRadians(startAngle + HEX));
+//			connectionPoints[i] = newX;
+//			connectionPoints[i + 1] = newY;
+//			startAngle += HEX;
+//		}
 		
-		for (int i = 0; i < POINTS; i +=2) {
-			double newX = points[i] + side * Math.cos(Math.toRadians(startAngle+HEX));
-			double newY = points[i+1] - side * Math.sin(Math.toRadians(startAngle+HEX));
-			connectionPoints[i] = newX;
-			connectionPoints[i + 1] = newY;
-			startAngle += HEX;
-		}
-//		System.out.println(Arrays.toString(connectionPoints));
+		// System.out.println(Arrays.toString(connectionPoints));
 		hexagon = new Polygon(points);
 		hexagon.setFill(Color.WHITE);
 		hexagon.setStroke(Color.BLACK);
 	}
 
-//	public static void main(String[] args) {
-//		HexTile h = new HexTile(10, 10, 10);
-//	}
-	public Polygon getHexagon(){
+	public Polygon getHexagon() {
 		return hexagon;
 	}
-	
-	public double[] getConnections(){
+
+	public double[] getConnections() {
 		return connectionPoints;
+	}
+	
+	public void setHandler(EventHandler<MouseEvent> e){
+		hexagon.setOnMouseClicked(e);
 	}
 
 }
