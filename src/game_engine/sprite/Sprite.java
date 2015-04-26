d2dca17f325fb31bef2f03accb7ba401b3e35819
BrianLavallee <bpl6@duke.edu>
2015-04-25 22:02:30 -0400

    need to restart my computer

--- src/game_engine/sprite/Sprite.java
+++ src/game_engine/sprite/Sprite.java
@@ -2,21 +2,8 @@ package game_engine.sprite;
 
 import game_engine.behaviors.IAction;
 import game_engine.behaviors.IActor;
-<<<<<<< HEAD
 import game_engine.physics.Vector;
 import game_engine.physics.objects.PhysicsObject;
-=======
-<<<<<<< HEAD
-import game_engine.physics.Material;
-import game_engine.physics.PhysicsEngine;
-import game_engine.physics.PhysicsObject;
-import game_engine.physics.RigidBody.RBodyType;
-=======
-import game_engine.physics_engine.Vector;
-import game_engine.physics_engine.physics_object.IPhysicsObject;
->>>>>>> 22264357c8c80e754b05e8fb37839b38026dd385
-import game_player.Animation;
->>>>>>> 76ac245ad5aa02e6c3f45238f1cc6763a4d3c008
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,17 +12,6 @@ import java.util.Observable;
 import javafx.scene.image.ImageView;
 
 public class Sprite extends Observable implements IActor {
-<<<<<<< HEAD
-=======
-	private int myId;
-	private String myName;
-	private String myState;
-	private Animation myAnimation;
-<<<<<<< HEAD
-	protected PhysicsObject myPhysicsObject;
-	private Map<String, IBehavior> myBehaviorMap = new HashMap<>();
-//	private HitBox myHitBox;
->>>>>>> 76ac245ad5aa02e6c3f45238f1cc6763a4d3c008
 	
 	private String state;
 	private Animation animation;
@@ -52,60 +28,6 @@ public class Sprite extends Observable implements IActor {
 		setChanged();
 		notifyObservers();
 		
-<<<<<<< HEAD
-=======
-	}
-	
-	/**
-	 * Blank Constructor
-	 */
-	public Sprite(PhysicsObject physics) {
-		// TODO
-	    myPhysicsObject = physics;
-	    myId = 0;
-	    myAnimation = new Animation(this,myPhysicsObject);
-	}
-	
-	/**
-	 * Constructor Sprite
-	 * Creates sprite object with a defined name
-	 * @param name the string to name the sprite
-	 */
-	public Sprite(PhysicsObject physics, String name){
-	    myPhysicsObject = physics;
-	    myId = 0; //TODO make call to SpriteManager to get unique ID or don't allow sprite to constructed without ID
-	    myName = name;
-	    myAnimation = new Animation(this,myPhysicsObject);
-	}
-	
-	/**
-	 * Constructor Sprite
-	 * Creates sprite object with a defined name and specified id
-	 * @param name the string to name the sprite
-	 * @param id the id of the specific sprite
-	 */
-	public Sprite(PhysicsObject physics, String name, int id){
-	    myPhysicsObject = physics;
-	    myName = name;
-	    myId = id;
-	    myAnimation = new Animation(this,myPhysicsObject);
-	}
-	
-	/**
-	 * method update
-	 * Updates the sprite
-	 */
-	public abstract void update();
-=======
-	protected IPhysicsObject myPhysicsObject;
-	private Map<String, IAction> myActionMap = new HashMap<>();
->>>>>>> 22264357c8c80e754b05e8fb37839b38026dd385
-	
-	public Sprite(String state, Animation animation, IPhysicsObject physicsObject) {
-		myState = state;
-		myAnimation = animation;
-		myPhysicsObject = physicsObject;
->>>>>>> 76ac245ad5aa02e6c3f45238f1cc6763a4d3c008
 		buildActionMap();
 	}
 	
@@ -139,40 +61,6 @@ public class Sprite extends Observable implements IActor {
 		return state;
 	}
 
-<<<<<<< HEAD
-=======
-	public void setID(int id){
-	    myId = id;
-	}
-	
-	public double getID(){
-	    return myId;
-	}
-
-	public void setName(String name){
-	    this.myName = name;
-	}
-	
-	public String getName(){
-	    return this.myName;
-	}
-	
-<<<<<<< HEAD
-//	public HitBox getHitBox(){
-//	    return myHitBox;
-//	}
-	
-	public void setPhysicsObject(PhysicsObject physicsObject){
-	    myPhysicsObject = physicsObject;
-=======
-	public void setStateName(String movementName){
-		myState = movementName;
-		notifyObservers(myState);
->>>>>>> 22264357c8c80e754b05e8fb37839b38026dd385
-	}
-	
-	@IActionAnnotation(numParams = 2, description = "moving forward/backward")
->>>>>>> 76ac245ad5aa02e6c3f45238f1cc6763a4d3c008
 	private IAction moveForward = (params) -> {
 		physicsObject.applyImpulse(new Vector(Double.parseDouble(params[0]), Double.parseDouble(params[1])));
 	};
