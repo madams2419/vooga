e89e9044049319d5e42b360f28260d4d5ea42d40
Kevinchang1214 <kgc12@duke.edu>
2015-04-27 00:53:11 -0400

    javadoced transitionManager class

--- src/game_engine/sprite/TransitionManager.java
+++ src/game_engine/sprite/TransitionManager.java
@@ -24,110 +24,149 @@ import javafx.scene.shape.Path;
 import javafx.scene.shape.PathElement;
 import javafx.scene.shape.Shape;
 import javafx.util.Duration;
+
+
 /**
  * 
  * @author Kevin Chang
- * Class to manage Transitions for all sprites
+ *         Class to manage Transitions for all sprites
  */
 public class TransitionManager {
     private Group myGroup;
-    private Map<Sprite,List<PathElement>> myTransitionMap;
+    private Map<Sprite, List<PathElement>> myTransitionMap;
     private List<SequentialTransition> myTransitions;
     private List<Sprite> mySprites;
     private List<String[]> myParams;
-    
+
     private ArrayList<Integer> myDuration;
     private ArrayList<Integer> myDelay;
-    
-   public TransitionManager(Group group, ArrayList<Sprite> sprites,ArrayList<String[]> params,ArrayList<Integer> duration,
-                            ArrayList<Integer> delay){
-       myGroup = group;
-       myTransitionMap = new HashMap<Sprite,List<PathElement>>();
-       myTransitions = new ArrayList<SequentialTransition>();
-       mySprites = sprites;
-       myParams = params;
-       myDuration = duration;
-       myDelay = delay;
-   }
-   
-   public List<String[]> getParams(){
-       return myParams;
-   }
-   
-   public List<SequentialTransition> getTransitions(){
-       return myTransitions;
-   }
-   
-   public void initialize(){
-      
-       for(int i = 0;i<mySprites.size();i++){
-           
-           createTransitions(mySprites.get(i),myParams.get(i));
-           Sprite sprite = mySprites.get(i);
-           myGroup.getChildren().add(sprite.getImageView());
-           Path path = new Path();
-           path.getElements().addAll(myTransitionMap.get(sprite));
-           sprite.getPhysicsObject().ignorePhysics(sprite.getAnimation());
-           System.out.println("TIME PARAMS:"+myDuration+ " "+myDelay);
-           SequentialTransition pt = initializePath((Node) sprite.getImageView(),path,myDuration.get(i),myDelay.get(i));
-           myTransitions.add(pt);
-           
-           System.out.println(myParams.get(i).length);
-          for(int count = 0;count<myParams.get(i).length;count++){
-              System.out.println(myParams.get(i)[count]);
-          }
-       }
-   }
-   
-   public void playTransitions(){
-       
-       myTransitions.forEach(transition ->{
-           transition.play();
-       });
-   }
-   
-   private SequentialTransition initializePath (Node object, Path path, int seconds,int delay) {
-       PathTransition pathTr = new PathTransition();
-       pathTr.setDuration(Duration.seconds(seconds));
-       pathTr.setPath(path);
-       pathTr.setNode(object);
-       pathTr.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-       pathTr.setAutoReverse(true);
-       
-       PauseTransition pause = new PauseTransition(Duration.seconds(delay));
-       pause.setAutoReverse(true);
 
-      SequentialTransition seqTran = new SequentialTransition(pause,pathTr);
-      seqTran.setCycleCount(Timeline.INDEFINITE);
-      seqTran.setAutoReverse(true);
-      
-       return seqTran;
-
-   }
-   
-   private void createTransitions(Sprite sprite, String[]param){
-       ArrayList<PathElement> pathEl = new ArrayList<>();
-       SimpleDoubleProperty[] parameters = new SimpleDoubleProperty[param.length];
-       
-       for(int i = 0;i<param.length;i++){
-           SimpleDoubleProperty temp = new SimpleDoubleProperty();
-          temp.set(Double.parseDouble(param[i]));
-          parameters[i]=temp;
-          
-       }
+    public TransitionManager (Group group,
+                              ArrayList<Sprite> sprites,
+                              ArrayList<String[]> params,
+                              ArrayList<Integer> duration,
+                              ArrayList<Integer> delay) {
+        myGroup = group;
+        myTransitionMap = new HashMap<Sprite, List<PathElement>>();
+        myTransitions = new ArrayList<SequentialTransition>();
+        mySprites = sprites;
+        myParams = params;
+        myDuration = duration;
+        myDelay = delay;
+    }
     
-       MoveTo start = new MoveTo(parameters[0].doubleValue(),parameters[1].doubleValue());
-       CubicCurveTo lineFollow = new CubicCurveTo();
-       lineFollow.setControlX1(parameters[2].doubleValue());
-       lineFollow.setControlY1(parameters[3].doubleValue());
-       lineFollow.setControlX2(parameters[4].doubleValue());
-       lineFollow.setControlY2(parameters[5].doubleValue());
-       lineFollow.setX(parameters[6].doubleValue());
-       lineFollow.setY(parameters[7].doubleValue());
-       pathEl.add(start);
-       pathEl.add(lineFollow);
-       
-       myTransitionMap.put(sprite, pathEl);     
-};
+    /**
+     * method getParams
+     * @return The list of parameters for all the sprites which include path following
+     */
+    public List<String[]> getParams () {
+        return myParams;
+    }
+
+    /**
+     * method getTransitions
+     * @return the list of transitions that need to be executed
+     */
+    public List<SequentialTransition> getTransitions () {
+        return myTransitions;
+    }
+    
+    //Create all the transitions and add to list
+    /**
+     * method initialize
+     * initializes elements of the path and makes appropriate calls to 
+     * create all transitions
+     */
+    public void initialize () {
+
+        for (int i = 0; i < mySprites.size(); i++) {
+
+            createTransitions(mySprites.get(i), myParams.get(i));
+            Sprite sprite = mySprites.get(i);
+            myGroup.getChildren().add(sprite.getImageView());
+            Path path = new Path();
+            path.getElements().addAll(myTransitionMap.get(sprite));
+            sprite.getPhysicsObject().ignorePhysics(sprite.getAnimation());
+            SequentialTransition pt =
+                    initializePath((Node) sprite.getImageView(), path, myDuration.get(i),
+                                   myDelay.get(i));
+            myTransitions.add(pt);
+
+            System.out.println(myParams.get(i).length);
+            for (int count = 0; count < myParams.get(i).length; count++) {
+                System.out.println(myParams.get(i)[count]);
+            }
+        }
+    }
+
+    /**
+     * method playTransitions
+     * plays the transition in the list contained in transition manager
+     */
+    public void playTransitions () {
+
+        myTransitions.forEach(transition -> {
+            transition.play();
+        });
+    }
+
+    /**
+     * method initializePath
+     * @param object the object to be following the path
+     * @param path the path to be followed
+     * @param seconds the duration to complete one length of the path
+     * @param delay the time to pause before each path restarts
+     * @return a SequentialTransition that represents a pause transition and the
+     *          path transition to be followed
+     */
+    private SequentialTransition initializePath (Node object, Path path, int seconds, int delay) {
+        PathTransition pathTr = new PathTransition();
+        pathTr.setDuration(Duration.seconds(seconds));
+        pathTr.setPath(path);
+        pathTr.setNode(object);
+        pathTr.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTr.setAutoReverse(true);
+
+        PauseTransition pause = new PauseTransition(Duration.seconds(delay));
+        pause.setAutoReverse(true);
+
+        SequentialTransition seqTran = new SequentialTransition(pause, pathTr);
+        seqTran.setCycleCount(Timeline.INDEFINITE);
+        seqTran.setAutoReverse(true);
+
+        return seqTran;
+
+    }
+
+    /**
+     * method createTransitions
+     * @param sprite the sprite to link to the path
+     * @param param the parameters (properties) of the curve to be created and
+     *          subsequently linked to the sprite
+     */
+    private void createTransitions (Sprite sprite, String[] param) {
+        ArrayList<PathElement> pathEl = new ArrayList<>();
+        SimpleDoubleProperty[] parameters = new SimpleDoubleProperty[param.length];
+
+        for (int i = 0; i < param.length; i++) {
+            SimpleDoubleProperty temp = new SimpleDoubleProperty();
+            temp.set(Double.parseDouble(param[i]));
+            parameters[i] = temp;
+
+        }
+
+        MoveTo start = new MoveTo(parameters[0].doubleValue(), parameters[1].doubleValue());
+        CubicCurveTo lineFollow = new CubicCurveTo();
+        lineFollow.setControlX1(parameters[2].doubleValue());
+        lineFollow.setControlY1(parameters[3].doubleValue());
+        lineFollow.setControlX2(parameters[4].doubleValue());
+        lineFollow.setControlY2(parameters[5].doubleValue());
+        lineFollow.setX(parameters[6].doubleValue());
+        lineFollow.setY(parameters[7].doubleValue());
+        pathEl.add(start);
+        pathEl.add(lineFollow);
+
+        myTransitionMap.put(sprite, pathEl);
+    };
 
 }
