64620b9234189bba79df1ef9bb9371b8484bc728
Hojeannie Chung <hc114@duke.edu>
2015-05-03 22:31:33 -0400

    adding comments ont op of masterpiece

--- src/XML/QueryParser.java
+++ src/XML/QueryParser.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Jeannie Chung
+
 package XML;
 
 import java.io.File;
--- src/utilities/SocialCenter/Driver.java
+++ src/utilities/SocialCenter/Driver.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Jeannie Chung
+
 package utilities.SocialCenter;
 
 import java.sql.Connection;
